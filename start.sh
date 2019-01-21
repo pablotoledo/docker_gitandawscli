4c6c879852c02dcd942df2fea63f5c78d8274f93
root <juanpablo.gavagnin@vodafone.com>
2019-01-21 15:52:26 +0100

    .

--- start.sh
+++ start.sh
@@ -5,8 +5,8 @@ __create_user() {
 # Create a user to SSH into as.
 useradd slave
 SSH_USERPASS=slaveContainer
-echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin jenkins)
-echo ssh jenkins password: $SSH_USERPASS
+echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin slave)
+echo ssh slave password: $SSH_USERPASS
 }
 
 # Call all functions
